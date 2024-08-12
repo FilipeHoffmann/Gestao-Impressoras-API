dbd3c32547d4496c38bf9bdeef7d310005243257
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-08-12 16:24:13 -0300

    Fix the date

--- src/routes/contratos/contratos.js
+++ src/routes/contratos/contratos.js
@@ -6,7 +6,7 @@ const db = new Database();
 router.get('/', async (req, res) => {
     try {
         const connection = await db.getConnection();
-        const [rows] = await connection.execute('SELECT * FROM contratos');
+        const [rows] = await connection.execute("SELECT `contratos`.`idContrato`, DATE_FORMAT(`contratos`.`dataInicial`, '%Y-%m-%d') AS `dataInicial`, DATE_FORMAT(`contratos`.`dataFinal`, '%Y-%m-%d') AS `dataFinal`, DATE_FORMAT(`contratos`.`dataFinalAtual`, '%Y-%m-%d') AS `dataFinalAtual` FROM `gestaoimpressoras`.`contratos`;");
         res.json(rows);
     } catch (error) {
         console.error('Error fetching contratos:', error.message);
