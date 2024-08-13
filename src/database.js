0ed06656e482d2591239b55f12bf10c2df9c4239
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-08-13 13:28:52 -0300

    Updating database connection

--- src/database.js
+++ src/database.js
@@ -10,13 +10,18 @@ class Database {
             database: process.env.DB_NAME,
             waitForConnections: true,
             connectionLimit: 10,
-            queueLimit: 0
+            queueLimit: 0,
+            connectTimeout: 10000, 
         });
     }
 
     async getConnection() {
         try {
             const connection = await this.pool.getConnection();
+
+            
+            await connection.ping();
+
             console.log('Database connection established');
             return connection;
         } catch (error) {
@@ -26,20 +31,34 @@ class Database {
         }
     }
 
-    async reconnect() {
+    async reconnect(retryCount = 5) {
         console.log('Attempting to reconnect to the database...');
         try {
             await this.closePool();
-            this.pool = mysql.createPool({
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_NAME,
-                waitForConnections: true,
-                connectionLimit: 10,
-                queueLimit: 0
-            });
-            console.log('Reconnected to the database');
+
+            
+            while (retryCount > 0) {
+                try {
+                    this.pool = mysql.createPool({
+                        host: process.env.DB_HOST,
+                        user: process.env.DB_USER,
+                        password: process.env.DB_PASSWORD,
+                        database: process.env.DB_NAME,
+                        waitForConnections: true,
+                        connectionLimit: 10,
+                        queueLimit: 0,
+                        connectTimeout: 10000,
+                    });
+                    console.log('Reconnected to the database');
+                    return;
+                } catch (error) {
+                    console.error(`Retry attempt failed: ${5 - retryCount + 1}`);
+                    retryCount--;
+                    await this.delay(2000); 
+                }
+            }
+            console.error('All retry attempts failed');
+            throw new Error('Unable to reconnect to the database');
         } catch (error) {
             console.error('Error reconnecting to the database:', error.message);
             throw error;
@@ -54,6 +73,10 @@ class Database {
             console.error('Error closing database pool:', error.message);
             throw error;
         }
+    }
+
+    delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
 
